1d866f2a2da048811cbf2708847518a724ecaf94
Ryan Robinson <ryanrob0930@gmail.com>
2018-12-13 17:27:07 -0500

    main pag

---
+++ pokemon.js
@@ -0,0 +1,5 @@
+let groudon = document.querySelector('#groudon1');
+
+groudon.addEventListener('click', event =>{
+  groudon.href= "file:///Users/ryanrob0930icloud.com/Desktop/mystuff/Pokedex/index.html";
+})
