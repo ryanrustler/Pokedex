2d9ef3970f2c456d7917295f7e2ec0c3a73ffdc8
Ryan Robinson <ryanrob0930@gmail.com>
2018-12-14 12:56:58 -0500

    almost finish

--- pokemon.js
+++ pokemon.js
@@ -3,3 +3,38 @@ let groudon = document.querySelector('#groudon1');
 groudon.addEventListener('click', event =>{
   groudon.href= "file:///Users/ryanrob0930icloud.com/Desktop/mystuff/Pokedex/index.html";
 })
+
+let ho_oh = document.querySelector('#HO-OH');
+  ho_oh.addEventListener('click', event => {
+    ho_oh.href ="file:///Users/ryanrob0930icloud.com/Desktop/mystuff/Pokedex/index2.html";
+  })
+
+  let snorlax = document.querySelector('#snorlax');
+    snorlax.addEventListener('click', event => {
+      snorlax.href ="file:///Users/ryanrob0930icloud.com/Desktop/mystuff/Pokedex/index3.html";
+    }) 
+
+
+let pokeball = document.querySelector('#pokeball1');
+  pokeball.addEventListener('click', event =>{
+    event.preventDefault();
+    let pokemon1 = document.querySelector('#groudon1');{
+      pokemon1.style.display = "inline-block";
+    }
+  } )    
+
+  let pokeball2 = document.querySelector('#pokeball2');
+  pokeball2.addEventListener('click', event =>{
+    event.preventDefault();
+    let pokemon2 = document.querySelector('#HO-OH');{
+      pokemon2.style.display = "inline-block";
+    }
+  } )    
+
+  let pokeball3 = document.querySelector('#pokeball3');
+  pokeball3.addEventListener('click', event =>{
+    event.preventDefault();
+    let pokemon3 = document.querySelector('#snorlax');{
+      pokemon3.style.display = "inline-block";
+    }
+  } )    
