24e79c9c5587f255617ebe1c8b8469367287762e
Ryan Robinson <ryanrob0930@gmail.com>
2018-12-20 11:12:10 -0500

    change

--- pokemon.js
+++ pokemon.js
@@ -13,28 +13,28 @@ let ho_oh = document.querySelector('#HO-OH');
     snorlax.addEventListener('click', event => {
       snorlax.href ="file:///Users/ryanrob0930icloud.com/Desktop/mystuff/Pokedex/snorlax.html";
     }) 
-
+    
 
 let pokeball = document.querySelector('#pokeball1');
   pokeball.addEventListener('click', event =>{
     event.preventDefault();
-    let pokemon1 = document.querySelector('#groudon1');{
+    let pokemon1 = document.querySelector('#groudon1');
       pokemon1.style.opacity = 1;
-    }
-  } )    
+    
+   } )    
 
   let pokeball2 = document.querySelector('#pokeball2');
   pokeball2.addEventListener('click', event =>{
     event.preventDefault();
-    let pokemon2 = document.querySelector('#HO-OH');{
+    let pokemon2 = document.querySelector('#HO-OH');
       pokemon2.style.opacity = 1;;
-    }
+    
   } )    
 
   let pokeball3 = document.querySelector('#pokeball3');
   pokeball3.addEventListener('click', event =>{
     event.preventDefault();
-    let pokemon3 = document.querySelector('#snorlax');{
+    let pokemon3 = document.querySelector('#snorlax');
       pokemon3.style.opacity= 1;
-    }
+    
   } )    
